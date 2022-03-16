07319584698e947853a017d8bb7a838b5f2cc557
Julian Ramirez <jramirezg@eafit.edu.co>
2022-03-15 20:11:38 -0500

    Creada NavBar e integrada a React

---
+++ src/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {NavBar} from '../NavBar'
+//import './App.css';
+
+function App() {
+  return (
+
+    <React.Fragment>
+      <NavBar/>
+    </React.Fragment>
+
+  );
+}
+
+export default App;
