9c56ae76005e1566f51cb509448e2e3cc4304f12
Julian Ramirez <jramirezg@eafit.edu.co>
2022-03-22 18:08:34 -0500

    Calculo de cuotas

--- src/pages/Simulador/resultados.js
+++ src/pages/Simulador/resultados.js
@@ -7,12 +7,20 @@ function Resultados(){
     const datos = JSON.parse(localStorage.getItem("datos"));
     console.log(datos);
 
+    const value = datos.value;
+    const fee = datos.fee;
+    const numQuotas = datos.numQuotas;
+    const interestRate = datos.interestRate;
+    
+    const calc = value * (Math.pow(1+interestRate/100,numQuotas)*interestRate/100)/(Math.pow(1+interestRate/100,numQuotas)-1);
+    console.log(calc);
+
     return (
         <div>
-            {datos.value}
-            {datos.fee}
-            {datos.numQuotas}
-            {datos.interestRate}
+            {value}
+            {fee}
+            {numQuotas}
+            {interestRate}
         </div>
     );
 }
