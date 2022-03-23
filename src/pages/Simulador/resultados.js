1ab3426dfa6ae24c77cf2e4f90ca3541baef2e53
Liz Oriana Rodrigues Cruz <79216103+lizorc@users.noreply.github.com>
2022-03-22 19:56:37 -0500

    Actulización de la interfaz de resultados

--- src/pages/Simulador/resultados.js
+++ src/pages/Simulador/resultados.js
@@ -1,19 +1,42 @@
 import React from 'react';
-import styles from './simulador.module.css'
+import styles from './resultados.module.css'
 import { Link } from "react-router-dom";
-import {useState} from 'react'
+//import {useState} from 'react'
+//import { render } from '@testing-library/react';
 function Resultados(){
 
     const datos = JSON.parse(localStorage.getItem("datos"));
     console.log(datos);
 
+    
     return (
-        <div>
+        <div className={styles.formBody}>
+            <p className={styles.tituloResultado}>Resultado de la simulación</p>
+
+            <div className = {styles.margen}>
+                <div className = {styles.BoxWhite}>
+                    <h4>Titulo</h4>
+                    <p> Valor de la cuota</p>
+                    <span>
+                        {datos.value}
+                    </span>
+                    <p>Saldo</p>
+                    <span>
+                        {datos.fee}
+                    </span>
+                </div>
+            </div>
+
             {datos.value}
             {datos.fee}
             {datos.numQuotas}
             {datos.interestRate}
+
+            <div className={styles.resultField}>
+            <Link to="simulador"><input className={styles.submitButton} type="submit" value="Volver"/></Link>
+            </div>
         </div>
+        
     );
 }
 
