2a5a5eb1054897c31ea3db9ea6f2e1d224dea374
Simón Marín Giraldo <smaring1@eafit.edu.co>
2022-05-26 08:44:19 -0500

    Ver PDF's

--- frontend/src/components/VistaPDF/vistaPDF.js
+++ frontend/src/components/VistaPDF/vistaPDF.js
@@ -1,39 +1,39 @@
 import { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
 import React from "react";
 import Button from "react-bootstrap/Button";
+import VistaWeb from "./VistaWeb";
+import DocuPDF from "./DocuPDF";
 
-function VistaPDF() {
-    const [poema, setPoema] = React.useState("");
+function Vista() {
+
     const [verWeb, setVerWeb] = React.useState(false);
     const [verPDF, setVerPDF] = React.useState(false);
 
-    return ( 
+    const Menu = ()=> ( 
         <nav className="NavBoton">
             <Button
-            variant="dark">
+            variant="dark"
             onClick={()=>{
                 setVerWeb(!verWeb);
                 setVerPDF(false);
-            }}
+            }}>
             {verWeb ? "Ocultar Web" : "Ver Web"}
             </Button>
-
-            <Button
-            variant="dark">
-            onClick={()=>{
-                setVerWeb(!verWeb);
-                setVerPDF(false);
-            }}
-            {verPDF ? "Ocultar PDF" : "Ver PDF"}
-            </Button>
-
-            <PDFDownloadLink>
-            </PDFDownloadLink>
-
-
     
         </nav>
     )
+
+    return (
+        <div>
+            <Menu />
+            {verWeb ? <VistaWeb/> : null}
+            {verPDF ? (
+            <PDFViewer style={{ width: "100%", height: "90vh" }}>
+              <DocuPDF />
+            </PDFViewer>
+          ) : null}
+        </div>
+    )
 }
 
-export default VistaPDF
+export default Vista
